8bb77a216f4993026f33361b09a48920d0ad8a97
HuynhQui01 <109355369+HuynhQui01@users.noreply.github.com>
2023-04-22 14:06:11 +0700

    Add files via upload

---
+++ HuynhVanQui_GCC210153/src/Model/Account.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author MY LAPTOP
+ */
+public class Account implements Serializable{
+    private String usrName;
+    private String password;
+
+    public Account(String usrName, String password) {
+        this.usrName = usrName;
+        this.password = password;
+    }
+
+    public String getUsrName() {
+        return usrName;
+    }
+
+    public void setUsrName(String usrName) {
+        this.usrName = usrName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
